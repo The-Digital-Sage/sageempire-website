40bec6fec5dd3184170ad5f078ad6374ce2a3710
The-Digital-Sage <hyrschall@sageempire.tv>
2025-07-20 21:30:35 -0600

    Update main_simple.py
    Changed the end of the Python file by manus

--- main_simple.py
+++ main_simple.py
@@ -92,5 +92,7 @@ def serve(path):
             return "index.html not found", 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
